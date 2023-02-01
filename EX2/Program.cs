b4154844a489c2af4dcf65e4b0983094a3e37c1a
AlohaDance1337 <121186810+AlohaDance1337@users.noreply.github.com>
2023-02-01 21:19:46 +0300

    Update Program.cs

--- EX2/Program.cs
+++ EX2/Program.cs
@@ -1,9 +1,13 @@
-﻿Console.Write("Введите число: ");
+Console.Write("Введите число: ");
 String strValue = Console.ReadLine();
 int value = Convert.ToInt32(strValue);
-if (value>100)
-{
-    int s = (value % 100) / 10;
+if (value>=100)
+{   
+    while (value > 999)
+    {
+        value = value / 10;
+    }
+    int s = (value % 10);
     Console.Write($"Число под третьем номером: {s}");
 }
 else
