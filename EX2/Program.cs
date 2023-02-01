38957656d6fe63c069dea9f666d04312a37d1e3c
AlohaDance1337 <121186810+AlohaDance1337@users.noreply.github.com>
2023-02-01 21:06:28 +0300

    Add files via upload

---
+++ EX2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+String strValue = Console.ReadLine();
+int value = Convert.ToInt32(strValue);
+if (value>100)
+{
+    int s = (value % 100) / 10;
+    Console.Write($"Число под третьем номером: {s}");
+}
+else
+{
+    Console.Write("У числа отсутствет третья цифра");
+}
