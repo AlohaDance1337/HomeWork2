98ffb2a9b372a264db3c1e102c97c4991f447752
AlohaDance1337 <121186810+AlohaDance1337@users.noreply.github.com>
2023-02-01 20:57:57 +0300

    Add files via upload

---
+++ EX1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите трехзначное число: ");
+String strValue = Console.ReadLine();
+int value = Convert.ToInt32(strValue);
+
+int s = (value % 100) / 10;
+
+Console.Write($"Число под вторым номером {s}");
